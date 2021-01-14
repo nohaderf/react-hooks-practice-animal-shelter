a5a54c488d7e5e0cc39bcc678d1f27aa6c135a5f
nohaderf <freda.hon@gmail.com>
2021-01-13 22:46:41 -0500

    commit tree f4392e1fae4e944e5af17b74fe5d4680c4e176fa

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -2,8 +2,12 @@ import React from "react";
 
 import Pet from "./Pet";
 
-function PetBrowser() {
-  return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>;
+function PetBrowser({ pets, onAdoptPet }) {
+
+  const petInfo = pets.map((pet) => {
+    return <Pet key={pet.id} pet={pet} onAdoptPet={onAdoptPet} />
+  })
+  return <div className="ui cards">{petInfo}</div>
 }
 
 export default PetBrowser;
